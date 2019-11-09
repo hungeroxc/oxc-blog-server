0c8239d5525adedb5108cfb33257af39d3aa8bfc
oxc <524112857@qq.com>
2019-11-09 16:16:17 +0800

    init

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'chore',
+                'feat',
+                'fix',
+                'test',
+                'perf',
+                'merge',
+                'config',
+                'init',
+                'improvement',
+            ]
+        ]
+    }
+}

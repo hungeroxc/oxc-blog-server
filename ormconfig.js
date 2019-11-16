466ed0de201ebf1b20fdc997986a309dfbda63f3
oxc <524112857@qq.com>
2019-11-16 10:08:59 +0800

    config: 将数据库的账户密码等通过package.json设置环境变量传入进去,方便以后在服务器中直接使用环境变量作为数据库的账密

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,12 @@
+const env = process.env
+
 module.exports = {
     type: 'mysql',
     host: '0.0.0.0',
     port: 3306,
-    username: 'www',
-    password: 'www',
-    database: 'blogtest',
+    username: env.DBPASSWORD,
+    password: env.DBACCOUNT,
+    database: env.DBNAME,
     synchronize: true,
     logging: false,
     entities: ['src/entity/*{.ts,.js}'],

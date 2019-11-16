304b5bfdadae20573b92be259d3b22db5ad364d9
oxc <524112857@qq.com>
2019-11-16 16:48:49 +0800

    config: change config

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ const env = process.env
 
 module.exports = {
     type: 'mysql',
-    host: '0.0.0.0',
+    host: 'localhost',
     port: 3306,
     username: env.DBACCOUNT,
     password: env.DBPASSWORD,

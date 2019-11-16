7612bb1e369b8ec322d98dbc45b44c70406cdbdd
oxc <524112857@qq.com>
2019-11-16 17:56:33 +0800

    test: 测试是否解决编译后的文件创建表失败的问题

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,15 @@
-const env = process.env
-
 module.exports = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: env.DBACCOUNT,
-    password: env.DBPASSWORD,
-    database: env.DBNAME,
+    // username: env.DBACCOUNT,
+    // password: env.DBPASSWORD,
+    // database: env.DBNAME,
+    username: 'www',
+    password: 'www',
+    database: 'blogtest',
     synchronize: true,
     logging: false,
-    entities: ['src/entity/*{.ts,.js}'],
+    entities: [__dirname + '/src/entity/*{.ts,.js}'],
     timezone: 'Z'
 }

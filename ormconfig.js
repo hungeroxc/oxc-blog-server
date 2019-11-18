e24c379cb50359f5e5f03e5e4a55d6f10de7f120
oxc <524112857@qq.com>
2019-11-18 10:40:10 +0800

    test: 自动化部署

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,12 @@
+const env = process.env
+
 module.exports = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    // username: env.DBACCOUNT,
-    // password: env.DBPASSWORD,
-    // database: env.DBNAME,
-    username: 'www',
-    password: 'www',
-    database: 'blogtest',
+    username: env.DBACCOUNT,
+    password: env.DBPASSWORD,
+    database: env.DBNAME,
     synchronize: true,
     logging: false,
     entities: [__dirname + '/dist/entity/*{.ts,.js}'],

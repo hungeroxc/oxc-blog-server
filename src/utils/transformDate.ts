f1bf3ef6f25b9e264f6310762eb944d6d61bcde7
oxc <524112857@qq.com>
2019-11-28 17:41:11 +0800

    fix: value.split is not a function

--- src/utils/transformDate.ts
+++ src/utils/transformDate.ts
@@ -1,7 +1,7 @@
 export function getTimeTransformer() {
     return {
         from(value: string): string {
-            const s = !!value ? value.split('.')[0] : value
+            const s = typeof value === 'string' ? value.split('.')[0] : value
             return s
         },
         to(value: string): string {

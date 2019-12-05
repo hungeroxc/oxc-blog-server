345cf8602a0f6a6d951ad82fcf70b6f01ee5978d
oxc <524112857@qq.com>
2019-12-05 17:50:36 +0800

    test: 测试环境

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,13 @@
 const env = process.env
-const isDev = env.APP_ENV === 'development'
+const isDev = env.APP_ENV === 'qa'
 
 module.exports = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: env.DBACCOUNT,
-    password: env.DBPASSWORD,
-    database: env.DBNAME,
+    username: isDev ? env.TESTDBACCOUNT : env.DBACCOUNT,
+    password: isDev ? env.TESTDBPASSWORD : env.DBPASSWORD,
+    database: isDev ? env.TESTDBNAME : env.DBNAME,
     logging: false,
     synchronize: true,
     entities: isDev ? ['src/entity/*{.ts,.js}'] : [__dirname + '/dist/src/entity/*{.ts,.js}'],
